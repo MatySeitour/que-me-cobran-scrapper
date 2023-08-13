2b5e069e8c37b86145eeebd84b872f7479636072
Matias Seitour <matias.seitour01@gmail.com>
2023-08-13 17:43:27 -0300

    feat(index) add cron schedule services

--- index.js
+++ index.js
@@ -3,23 +3,88 @@ import netflix from "./netflix.js"
 import hbo from "./hbo.js"
 import amazon from "./amazon.js"
 import disney from "./disney.js"
+import paramount from "./paramount.js"
+import star from "./star.js"
+import crunchyroll from "./crunchyrolls.js"
+import spotify from "./spotify.js"
 
-
-cron.schedule("0 * * * *", async () => {
+cron.schedule("0 1 * * *", async () => {
     try {
-        console.log("inició")
+        console.log("inició netflix")
         await netflix();
-        console.log("netflix terminó")
+        console.log("netflix terminó", process.env.TZ(new Date().toString()))
     }
     catch (e) {
         console.error(e)
     }
 })
 
-cron.schedule("15 * * * *", async () => {
+cron.schedule("0 2 * * *", async () => {
     try {
+        console.log("inició hbo")
         await hbo();
         console.log("hbo terminó")
+    }
+    catch (e) {
+        console.error(e)
+    }
+})
+cron.schedule("0 3 * * *", async () => {
+    try {
+        console.log("inició amazon")
+        await amazon();
+        console.log("amazon terminó", process.env.TZ(new Date().toString()))
+    }
+    catch (e) {
+        console.error(e)
+    }
+})
+cron.schedule("0 4 * * *", async () => {
+    try {
+        console.log("inició disney")
+        await disney();
+        console.log("disney terminó", process.env.TZ(new Date().toString()))
+    }
+    catch (e) {
+        console.error(e)
+    }
+})
+cron.schedule("0 5 * * *", async () => {
+    try {
+        console.log("inició paramount")
+        await paramount();
+        console.log("paramount terminó", process.env.TZ(new Date().toString()))
+    }
+    catch (e) {
+        console.error(e)
+    }
+})
+cron.schedule("0 6 * * *", async () => {
+    try {
+        console.log("inició star")
+        await star();
+        console.log("star terminó", process.env.TZ(new Date().toString()))
+    }
+    catch (e) {
+        console.error(e)
+    }
+})
+cron.schedule("0 7 * * *", async () => {
+    try {
+        console.log("inició crunchyroll")
+        await crunchyroll();
+        console.log("crunchyroll terminó", process.env.TZ(new Date().toString()))
+    }
+    catch (e) {
+        console.error(e)
+    }
+})
+
+cron.schedule("0 23 * * *", async () => {
+    try {
+        console.log("inició spotify")
+        await spotify();
+        console.log("spotify terminó", process.env.TZ(new Date().toString()))
     }
     catch (e) {
         console.error(e)
