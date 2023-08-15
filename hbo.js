df5550cac7fd2631d1af45fe19002b59bc829278
Matias Seitour <matias.seitour01@gmail.com>
2023-08-15 18:35:44 -0300

    test

--- hbo.js
+++ hbo.js
@@ -15,89 +15,11 @@ const hbo = async () => {
     try {
         const page = await browser.newPage();
         await onlyHtml(page);
-        await page.goto("https://www.hbomax.com/ar/es");
-        await page.waitForSelector(".plan-price");
-        const result = await page.evaluate((prices) => {
-            let arr = [];
-            const pricesNetflix = document.querySelectorAll(prices);
-            for (const price of pricesNetflix) {
-                arr.push(Array(price.innerText));
-            }
+        await page.goto("https://www.hbomax.com/ar/es", { waitUntil: 'networkidle0' }, { timeout: 1000 });
+        const data = await page.evaluate(() => document.querySelector('*').outerHTML);
 
-            return arr.filter(
-                (item, index) => index === 3 || index === 4 || index === 5
-            );
-        }, ".plan-price");
-
-        await browser.close()
-        const data = result.map((item, index) => {
-            if (index == 0) {
-                return {
-                    name: "1 mes",
-                    id: 48,
-                    price: Array(item)
-                        .join("")
-                        .split("$")[1]
-                        .split(/\s+/)
-                        .join("")
-                        .replace(",", ".")
-                        .split(".00")
-                        .join(""),
-                    benefits:
-                        "Puedes ver en 3 dispositivos a la vez.Hasta 5 perfiles para toda la familia.",
-                };
-            } else if (index == 1) {
-                return {
-                    name: "3 mes",
-                    id: 49,
-                    price: Array(item)
-                        .join("")
-                        .split("$")[1]
-                        .split(/\s+/)
-                        .join("")
-                        .replace(",", ".")
-                        .split(".00")
-                        .join("")
-                        .split(".")
-                        .join(""),
-                    benefits:
-                        "Disfruta en todas tus pantallas.Contenido en alta definición y 4K.",
-                };
-            } else {
-                return {
-                    name: "12 meses",
-                    id: 50,
-                    price: Array(item)
-                        .join("")
-                        .split("$")[1]
-                        .split(/\s+/)
-                        .join("")
-                        .replace(",", ".")
-                        .split(".00")
-                        .join("")
-                        .split(".")
-                        .join(""),
-                    benefits:
-                        "Chromecast y Airplay disponibles.Descarga y disfruta donde sea.Ahorra 3 meses.",
-                };
-            }
-        });
-
-        log("antes de actualizar los datos de hbo en la db ")
-
-        for (const plan of data) {
-            console.log("empieza a ejecutar el prisma hbo")
-            await prisma.plan.update({
-                where: {
-                    id: plan.id,
-                },
-                data: {
-                    price: Number(plan.price),
-                },
-            });
-        }
-
-        console.log(data)
+        console.log(data);
+        await browser.close();
     }
     catch (e) {
         await discordMessage("HBO", e)
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import cron from "node-cron"
 import netflix from "./netflix.js"
-import hbo from "./hbo.js"
 import amazon from "./amazon.js"
 import disney from "./disney.js"
 import paramount from "./paramount.js"
@@ -8,94 +7,97 @@ import star from "./star.js"
 import crunchyroll from "./crunchyroll.js"
 import spotify from "./spotify.js"
 import discordMessage from "./utils/discord_message.js"
+import hbo from "./hbo.js"
 
-cron.schedule("0 1 * * *", async () => {
-    try {
-        console.log("inició netflix")
-        await netflix();
-        console.log("netflix terminó", process.env.TZ)
-    }
-    catch (e) {
-        await discordMessage("Netflix", e)
-        console.error(e)
-    }
-})
+hbo();
 
-cron.schedule("0 2 * * *", async () => {
-    try {
-        console.log("inició hbo")
-        await hbo();
-        console.log("hbo terminó")
-    }
-    catch (e) {
-        await discordMessage("HBO", e)
-        console.error(e)
-    }
-})
-cron.schedule("0 3 * * *", async () => {
-    try {
-        console.log("inició amazon")
-        await amazon();
-        console.log("amazon terminó", process.env.TZ)
-    }
-    catch (e) {
-        await discordMessage("Amazon", e)
-        console.error(e)
-    }
-})
-cron.schedule("0 4 * * *", async () => {
-    try {
-        console.log("inició disney")
-        await disney();
-        console.log("disney terminó", process.env.TZ)
-    }
-    catch (e) {
-        await discordMessage("Disney+", e)
-        console.error(e)
-    }
-})
-cron.schedule("0 5 * * *", async () => {
-    try {
-        console.log("inició paramount")
-        await paramount();
-        console.log("paramount terminó", process.env.TZ)
-    }
-    catch (e) {
-        await discordMessage("Paramount+", e)
-        console.error(e)
-    }
-})
-cron.schedule("0 6 * * *", async () => {
-    try {
-        console.log("inició star")
-        await star();
-        console.log("star terminó", process.env.TZ)
-    }
-    catch (e) {
-        await discordMessage("Star+", e)
-        console.error(e)
-    }
-})
-cron.schedule("0 7 * * *", async () => {
-    try {
-        console.log("inició crunchyroll")
-        await crunchyroll();
-        console.log("crunchyroll terminó", process.env.TZ)
-    }
-    catch (e) {
-        await discordMessage("Crunchyroll", e)
-        console.error(e)
-    }
-})
+// cron.schedule("0 1 * * *", async () => {
+//     try {
+//         console.log("inició netflix")
+//         await netflix();
+//         console.log("netflix terminó", process.env.TZ)
+//     }
+//     catch (e) {
+//         await discordMessage("Netflix", e)
+//         console.error(e)
+//     }
+// })
 
-cron.schedule("0 23 * * *", async () => {
-    try {
-        console.log("inició spotify")
-        await spotify();
-        console.log("spotify terminó", process.env.TZ)
-    }
-    catch (e) {
-        await discordMessage("Spotify", e)
-        console.error(e)
-    }
-})
+// cron.schedule("0 2 * * *", async () => {
+//     try {
+//         console.log("inició hbo")
+//         await hbo();
+//         console.log("hbo terminó")
+//     }
+//     catch (e) {
+//         await discordMessage("HBO", e)
+//         console.error(e)
+//     }
+// })
+// cron.schedule("0 3 * * *", async () => {
+//     try {
+//         console.log("inició amazon")
+//         await amazon();
+//         console.log("amazon terminó", process.env.TZ)
+//     }
+//     catch (e) {
+//         await discordMessage("Amazon", e)
+//         console.error(e)
+//     }
+// })
+// cron.schedule("0 4 * * *", async () => {
+//     try {
+//         console.log("inició disney")
+//         await disney();
+//         console.log("disney terminó", process.env.TZ)
+//     }
+//     catch (e) {
+//         await discordMessage("Disney+", e)
+//         console.error(e)
+//     }
+// })
+// cron.schedule("0 5 * * *", async () => {
+//     try {
+//         console.log("inició paramount")
+//         await paramount();
+//         console.log("paramount terminó", process.env.TZ)
+//     }
+//     catch (e) {
+//         await discordMessage("Paramount+", e)
+//         console.error(e)
+//     }
+// })
+// cron.schedule("0 6 * * *", async () => {
+//     try {
+//         console.log("inició star")
+//         await star();
+//         console.log("star terminó", process.env.TZ)
+//     }
+//     catch (e) {
+//         await discordMessage("Star+", e)
+//         console.error(e)
+//     }
+// })
+// cron.schedule("0 7 * * *", async () => {
+//     try {
+//         console.log("inició crunchyroll")
+//         await crunchyroll();
+//         console.log("crunchyroll terminó", process.env.TZ)
+//     }
+//     catch (e) {
+//         await discordMessage("Crunchyroll", e)
+//         console.error(e)
+//     }
+// })
+
+// cron.schedule("0 23 * * *", async () => {
+//     try {
+//         console.log("inició spotify")
+//         await spotify();
+//         console.log("spotify terminó", process.env.TZ)
+//     }
+//     catch (e) {
+//         await discordMessage("Spotify", e)
+//         console.error(e)
+//     }
+// })
