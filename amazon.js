ee1bf10a03b40cca35f8b66309ae815637ca57cf
Matias Seitour <matias.seitour01@gmail.com>
2023-12-16 01:02:01 -0300

    fix(amazon) add new route to scrap data

--- amazon.js
+++ amazon.js
@@ -16,22 +16,25 @@ const amazon = async () => {
     try {
         const page = await browser.newPage();
         await onlyHtml(page)
-        await page.goto("https://selectra.com.ar/streaming/amazon-prime-video");
+        await page.goto("https://www.primevideo.com/offers/nonprimehomepage/ref=dvm_pds_brd_ar_hk_s_g_mkw_st1neeu6k-dc_pcrid_645960031810?mrntrk=slid__pgrid_41343053246_pgeo_1000058_x__adext__ptid_kwd-314575875558");
         await sleep(2000);
         await page.waitForSelector(
-            "#content-with-summary > div:nth-child(1) > p:nth-child(10) > strong"
+            "div > div > div > p"
         );
+
         const result = await page.evaluate((prices) => {
             let arr = [];
             const textAmazon = document.querySelector(prices)?.innerHTML;
             arr.push(textAmazon);
             return arr;
-        }, "#content-with-summary > div:nth-child(1) > p:nth-child(10) > strong");
+        }, "div > div > div > p");
+
+        console.log(result)
 
         browser.close();
 
         const getMonthPrice = Number(
-            Array(Array(result).join("").split("$")[1]).join("").split("mensuales")[0]
+            Array(Array(result).join("").split(";")[1]).join("").split("/mes")[0]
         );
         const getYearPrice = getMonthPrice * 12;
         const plans = [
