af6eac66627dd145a6927ccf4fcf4e9cba1bb862
Matias Seitour <matias.seitour01@gmail.com>
2023-12-16 17:11:54 -0300

    feat(hbo) replace url to hbo main page

--- hbo.js
+++ hbo.js
@@ -17,8 +17,8 @@ const hbo = async () => {
     try {
         const page = await browser.newPage();
         await onlyHtml(page);
-        await page.goto("https://selectra.com.ar/streaming/hbo-max");
-        await page.waitForSelector(".content-offer__figure");
+        await page.goto("https://www.hbomax.com/ar/es");
+        await page.waitForSelector("div > h3");
         const result = await page.evaluate((prices) => {
             let arr = [];
             const pricesNetflix = document.querySelectorAll(prices);
@@ -27,62 +27,36 @@ const hbo = async () => {
                 arr.push(price.innerHTML);
             }
             return arr;
-        }, ".content-offer__figure");
+        }, "div > h3");
 
 
         await browser.close()
-        const data = result.map((item, index) => {
-            if (index == 0) {
-                return {
-                    name: "1 mes",
-                    id: 48,
-                    price: Array(item)
-                        .join("")
-                        .split("$")[1]
-                        .split(/\s+/)
-                        .join("")
-                        .replace(",", ".")
-                        .split(".00")
-                        .join(""),
-                    benefits:
-                        "Puedes ver en 3 dispositivos a la vez.Hasta 5 perfiles para toda la familia.",
-                };
-            } else if (index == 1) {
-                return {
-                    name: "3 mes",
-                    id: 49,
-                    price: Array(item)
-                        .join("")
-                        .split("$")[1]
-                        .split(/\s+/)
-                        .join("")
-                        .replace(",", ".")
-                        .split(".00")
-                        .join("")
-                        .split(".")
-                        .join(""),
-                    benefits:
-                        "Disfruta en todas tus pantallas.Contenido en alta definición y 4K.",
-                };
-            } else {
-                return {
-                    name: "12 meses",
-                    id: 50,
-                    price: Array(item)
-                        .join("")
-                        .split("$")[1]
-                        .split(/\s+/)
-                        .join("")
-                        .replace(",", ".")
-                        .split(".00")
-                        .join("")
-                        .split(".")
-                        .join(""),
-                    benefits:
-                        "Chromecast y Airplay disponibles.Descarga y disfruta donde sea.Ahorra 3 meses.",
-                };
-            }
-        });
+
+        const priceMonth = Number(result[0].split(",")[0].replace(".", "").split("$")[1])
+        const priceYear = Number(result[1].split(",")[0].replace(".", "").split("$")[1])
+
+        const data = [{
+            name: "1 mes",
+            id: 48,
+            price: priceMonth,
+            benefits:
+                "Puedes ver en 3 dispositivos a la vez.Hasta 5 perfiles para toda la familia.",
+        },
+        {
+            name: "3 meses",
+            id: 49,
+            price: priceMonth * 3,
+            benefits:
+                "Disfruta en todas tus pantallas.Contenido en alta definición y 4K.",
+        },
+        {
+            name: "1 año",
+            id: 50,
+            price: priceYear,
+            benefits:
+                "Chromecast y Airplay disponibles.Descarga y disfruta donde sea.Ahorra 3 meses.",
+        }
+        ]
 
         log("antes de actualizar los datos de hbo en la db ")
 
@@ -107,5 +81,7 @@ const hbo = async () => {
     }
 
 }
+
+hbo()
 
 export default hbo;
