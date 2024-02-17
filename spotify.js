fcade950c8f9dcd80d2681fde27e4dddc6b880f4
Matias Seitour <matias.seitour01@gmail.com>
2024-02-17 14:29:24 -0300

    fix(spotify) fix selector prices

--- spotify.js
+++ spotify.js
@@ -19,7 +19,7 @@ const spotify = async () => {
         await onlyHtml(page)
         await page.goto("https://www.spotify.com/ar/premium/?utm_source=ar-en_brand_contextual_text&utm_medium=paidsearch&utm_campaign=alwayson_latam_ar_premiumbusiness_core_brand+contextual-desktop+text+broad+ar-en+google&gad=1&gclid=CjwKCAjw_uGmBhBREiwAeOfsdyvpaI79rTdGCXvLQtAbTrYLxkQwf0DrjD-BdlgLhaEO5OPmrtX3IBoCAUsQAvD_BwE&gclsrc=aw.ds");
         await sleep(2000);
-        await page.waitForSelector("#plans > article > div > div > p");
+        await page.waitForSelector("#plans > article > div > div > div > p");
         const result = await page.evaluate((prices) => {
             let arr = [];
             const pricesNetflix = document.querySelectorAll(prices);
@@ -27,49 +27,56 @@ const spotify = async () => {
                 arr.push(price.innerText);
             }
             return arr;
-        }, "#plans > article > div > div > p");
+        }, "#plans > article > div > div > div > p");
 
         await browser.close()
 
 
-        const getPrices = result.filter((item, index) => {
-            if (index === 0 || index === 3 || index === 6 || index === 9) {
-                return item.includes("$")
-            }
+        const getPrices = result.filter((item) => {
+            return item.includes("$")
         })
 
 
-        const data = getPrices.map((price, index) => {
+        const pricesBeforeOrder = getPrices.map((price) => {
+            return Number(price.split("*")[0].split("$")[1].split(" ").join("").replace(".", ""))
+        })
+
+        const pricesSpotify = pricesBeforeOrder.sort(function (a, b) { return a - b });
+
+        console.log(pricesSpotify);
+
+
+        const data = pricesSpotify.map((price, index) => {
             let plan = {
                 name: "",
                 price: 0,
                 benefits: "",
                 id: 0,
             };
-            if (index === 0) {
+            if (index === 1) {
                 plan.name = "Individual",
-                    plan.price = Number(price.split("*")[0].split("$")[1].split(" ").join("").split(",")[0].split(/\s+/)[1])
+                    plan.price = price
                 plan.benefits = "Música sin anuncios para 1 cuenta.Escuchá tus canciones en cualquier lugar, incluso sin conexión.Reproducción on-demand."
                 plan.id = 76
                 return plan;
             }
-            if (index === 1) {
+            if (index === 2) {
                 plan.name = "Duo",
-                    plan.price = Number(price.split("*")[0].split("$")[1].split(" ").join("").split(",")[0].split(/\s+/)[1]),
+                    plan.price = price,
                     plan.benefits = "Todos los beneficios del plan anterior.Disponible en 2 cuentas.Reproducción de música en modo offline."
                 plan.id = 77
                 return plan;
             }
-            if (index === 2) {
+            if (index === 3) {
                 plan.name = "Familiar",
-                    plan.price = Number(price.split("*")[0].split("$")[1].split(" ").join("").split(",")[0].split(/\s+/)[1]),
+                    plan.price = price,
                     plan.benefits = "Todos los beneficios del plan anterior.Disponible en 6 cuentas.Bloqueo de música con contenido explícito.Spotify Kids: una aplicación independiente creada solo para niños."
                 plan.id = 78
                 return plan;
             }
-            if (index === 3) {
+            if (index === 0) {
                 plan.name = "Estudiantes",
-                    plan.price = Number(price.split("*")[0].split("$")[1].split(" ").join("").split(",")[0].split(/\s+/)[1]),
+                    plan.price = price,
                     plan.benefits = "Descuento especial para estudiantes universitarios que cumplan con los requisitos.Todos los beneficios del plan individual."
                 plan.id = 79
                 return plan;
@@ -83,7 +90,7 @@ const spotify = async () => {
                     id: plan.id,
                 },
                 data: {
-                    price: Number(plan.price),
+                    price: plan.price,
                 },
             });
         }
@@ -97,4 +104,5 @@ const spotify = async () => {
     }
 }
 
+spotify();
 export default spotify;
